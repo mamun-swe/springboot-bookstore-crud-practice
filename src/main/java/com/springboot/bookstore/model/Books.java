a7012938af577c2d951ca36075075240a87ab08f
mamun-swe <mamun.swe.277@gmail.com>
2023-09-17 17:44:50 +0600

    Trying to adding global validator

--- src/main/java/com/springboot/bookstore/model/Books.java
+++ src/main/java/com/springboot/bookstore/model/Books.java
@@ -6,9 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-@Entity
 @Table(name = "books")
+@Entity
+@Data
+@Accessors(chain = true)
 public class Books {
 
     @Id
--- src/main/java/com/springboot/bookstore/model/dtos/BooksDto.java
+++ src/main/java/com/springboot/bookstore/model/dtos/BooksDto.java
@@ -1,12 +1,19 @@
 package com.springboot.bookstore.model.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class BooksDto {
-    private Long id;
+    @NotEmpty(message = "The name is required.")
     private String name;
+
+    @NotEmpty(message = "The title is required.")
     private String title;
+
+    @NotEmpty(message = "The author is required.")
     private String author;
+
+    @NotEmpty(message = "The price is required.")
     private Long price;
 }
